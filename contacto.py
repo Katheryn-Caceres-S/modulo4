ad828110cd5a6091c31133adc3b97cac2168d1e9
Katheryn-Caceres-S <kcs.ingcivil@gmail.com>
2025-06-11 01:55:35 +0000

    blabla

--- contacto.py
+++ contacto.py
@@ -1,3 +1,5 @@
+import os
+
 #creacion de clases
 #el doble guion bajo entrega la seguridad
 
@@ -22,6 +24,8 @@ class Contacto:
     def __repr__(self):
         return repr([self.nombre,self.email,self.telefono])
     
+
+    #(contacto) dice que hereada cosas de la clase contacto
 class ContactoTrabajo(Contacto):
 
     #constructor del ContactoTrabajo()
@@ -42,3 +46,110 @@ class ContactoTrabajo(Contacto):
     #esto es un metodo
     def __repr__(self):
         return repr([f"{self.nombre},{self.email},{self.telefono},{self.empresa},{self.oficio}"])
+
+#esta clase     
+class Manejador:
+
+    def __init__(self):
+        self.contactos:list[Contacto] = []
+
+    def agregar_contacto(self, contacto:Contacto):
+        self.contactos.append(contacto)
+
+    def listar_contacto(self)-> list [Contacto]:
+        return self.contactos
+    
+    def buscar_contacto(self, nombre):
+        nombre_minuscula = nombre.lower()
+        for i in self.contactos:
+            if i.nombre.lower() == nombre_minuscula:
+                return i
+        return None
+    
+    def borrar_contacto(self,nombre):
+        contacto=self.buscar_contacto(nombre)
+        if contacto is not None:
+            self.contactos.remove(contacto) 
+
+
+
+class Consola_menu:
+    #1 limpiar la consola
+    #2 mostrar menu
+
+    @staticmethod
+    def limpiar_consola():
+        #esta linea de menu es genericaa, siempre igual
+        os.system("cls" if os.name =="nt" else "clear")
+
+    @staticmethod
+    def mostrar_menu():
+        print("""
+== menu contactos ==
+1. agregar contacto
+2. agrgar contacto trabajo
+3. listar contactos   
+4. buscar usuario
+5. actualizar usuario
+6.eliminar usuario
+7. salir
+""")
+
+#no es necesario el print para usar la linea siguiente
+#Consola_menu.limpiar_consola()
+
+class Pedir_datos():
+
+    @staticmethod
+    def contacto_normal():
+        nombre = input("nombre: ")
+        email = input("email: ")
+        telefono = input("telefono: ")
+        return Contacto(nombre,email,telefono)
+    
+    @staticmethod
+    def contacto_empresa():
+        nombre = input("nombre: ")
+        email = input("email: ")
+        telefono = input("telefono: ")
+        empresa = input("email: ")
+        oficio = input("telefono: ")
+        return Contacto(nombre,email,telefono,empresa,oficio)
+
+class App:
+    def __init__(self):
+        self.manejador = Manejador()
+
+    def run(self):
+        while True:
+            Consola_menu.limpiar_consola()
+            Consola_menu.mostrar_menu()
+
+            opcion = input("elige un opcion: ")
+
+            if opcion == "1":
+                c = Pedir_datos.contacto_normal()
+                self.manejador.agregar_contacto(c)
+                print("contacto agregado!!")
+                input("enter oara continuar")
+            elif opcion == "2":
+                ct = Pedir_datos.contacto_empresa()
+                self.manejador.agregar_contacto(ct)
+                print("contacto agregado!!")
+                input("enter oara continuar")
+
+            elif opcion == "3":
+                #listar contacto
+                contactos = self.manejador.listar_contacto()
+                #esto es para preguntar si hay contactos
+                if contactos:
+                    #cara cada nombre en contactos imprimir
+                    for i in contactos:
+                        print(i)
+                else:
+                    print("sin contactos")
+            input("enter para continuar")
+
+
+app = App()
+app.run()
