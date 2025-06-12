20f40430906ca77e62abd01c89419df92e9f03a4
Katheryn-Caceres-S <kcs.ingcivil@gmail.com>
2025-06-12 01:21:42 +0000

    mmm

--- contacto.py
+++ contacto.py
@@ -5,20 +5,20 @@ import os
 
 class Contacto:
     def __init__(self, nombre:str, email:str, telefono:str) -> None:
-        self.__nombre =  nombre
-        self.__email = email
-        self.__telefono = telefono
+        self._nombre =  nombre
+        self._email = email
+        self._telefono = telefono
     
 #metodos con el decorador property. el interprete trata la fucnion como una variable
     @property
     def nombre(self):
-        return self.__nombre
+        return self._nombre
     @property
     def email(self):
-        return self.__email
+        return self._email
     @property
     def telefono(self):
-        return self.__telefono
+        return self._telefono
     
 #cambiar de formato de objeto a texto para que se pueda imprimir
     def __repr__(self):
@@ -32,22 +32,22 @@ class ContactoTrabajo(Contacto):
     def __init__(self, nombre, email, telefono, empresa, oficio):
         # contructor de Contacto ()
         super().__init__(nombre, email, telefono)
-        self.__empresa = empresa
-        self.__oficio = oficio
+        self._empresa = empresa
+        self._oficio = oficio
 
     @property
     def empresa(self):
-        return self.__empresa
+        return self._empresa
     
     @property
     def oficio (self):
-        return self.__oficio
+        return self._oficio
     
     #esto es un metodo
     def __repr__(self):
         return repr([f"{self.nombre},{self.email},{self.telefono},{self.empresa},{self.oficio}"])
 
-#esta clase     
+#esta clase  es el manejador   
 class Manejador:
 
     def __init__(self):
@@ -66,6 +66,16 @@ class Manejador:
                 return i
         return None
     
+    def actualizar_contacto(self, nombre , telefono , email):
+        c = self.buscar_contacto(nombre)
+        if c is not None:
+        
+            c._telefono = telefono
+            c._email = email
+            return c
+        return None
+   
+
     def borrar_contacto(self,nombre):
         contacto=self.buscar_contacto(nombre)
         if contacto is not None:
@@ -112,9 +122,9 @@ class Pedir_datos():
         nombre = input("nombre: ")
         email = input("email: ")
         telefono = input("telefono: ")
-        empresa = input("email: ")
-        oficio = input("telefono: ")
-        return Contacto(nombre,email,telefono,empresa,oficio)
+        empresa = input("empresa: ")
+        oficio = input("oficio: ")
+        return ContactoTrabajo(nombre,email,telefono,empresa,oficio)
 
 class App:
     def __init__(self):
@@ -131,12 +141,12 @@ class App:
                 c = Pedir_datos.contacto_normal()
                 self.manejador.agregar_contacto(c)
                 print("contacto agregado!!")
-                input("enter oara continuar")
+                
             elif opcion == "2":
                 ct = Pedir_datos.contacto_empresa()
                 self.manejador.agregar_contacto(ct)
                 print("contacto agregado!!")
-                input("enter oara continuar")
+
 
             elif opcion == "3":
                 #listar contacto
@@ -148,7 +158,37 @@ class App:
                         print(i)
                 else:
                     print("sin contactos")
-            input("enter para continuar")
+    
+
+            elif opcion == "4":
+                nombre = input("nombre contacto : ")
+                c = self.manejador.buscar_contacto(nombre)
+                if c:
+                     print(c)
+                else:
+                    print("contacto no encontrado!")
+
+
+            elif opcion == "5":
+                nombre = input("nombre a actualizar")
+                telefono = input("nuevo telefono")
+                email = input("nuevo correo")
+                c = self.manejador.actualizar_contacto(nombre, telefono, email)            
+                print("contacto actualizado")
+
+            elif opcion == "6":
+                nombre = input("contacto a borrar")
+                c = self.manejador.borrar_contacto(nombre)
+                print("contacto borrado")
+
+            elif opcion == "7":
+                Consola_menu.limpiar_consola()
+                print("adiossss")
+                exit()
+                
+
+            else:
+                print("opcion no valida")
 
 
 app = App()
